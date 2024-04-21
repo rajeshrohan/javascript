6409688e2790343fdfd8566be631254e72f5a04f
rajesh rohan <rajeshrohan62@gmail.com>
2024-04-21 11:54:24 +0530

    adding evenListners

---
+++ basic/EventListners/script.js
@@ -0,0 +1,34 @@
+
+// let count = 0;
+// document.getElementById("clickMe").addEventListener("click", function xyz(){
+//     console.log("button Clicked", ++count);
+// });
+            // same using closure, own scope
+
+// function attachEventListners(){
+//     let count = 0;
+//     document.getElementById("clickMe").addEventListener("click", function xyz(){
+//     console.log("button Clicked", ++count);
+//     });
+// }
+// attachEventListners();
+
+//--------------------------------------------------
+// document.getElementById("clickMe").addEventListener("click", function xyz(){
+//     console.log("Button Click");
+// })
+//---------------------------------------------
+// setTimeout(function(){
+//     console.log("timer");
+// }, 5000);
+
+// function x(y){	
+//     console.log("x");
+//     y();
+// }
+
+// x(function y(){	
+//     console.log("y");
+// })
+//------------------------------------------------
+
