a548f690e2932840f6ada8fd0556ff48debe0ae9
rajesh rohan <rajeshrohan62@gmail.com>
2024-04-30 16:53:07 +0530

    js engine

---
+++ basic/setTimeout/script.js
@@ -0,0 +1,14 @@
+console.log("star");
+setTimeout(function cb(){
+    console.log("callback");
+},5000);
+console.log("end");
+
+
+let startDate = new Date().getTime();
+let endDate = startDate;
+while(endDate < startDate + 10000){
+    endDate = new Date().getTime();
+}
+
+console.log("while expires");
